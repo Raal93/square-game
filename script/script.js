67096bd5c385a65d9a76da5f5ddf8869ba373981
Rafał Czajkowski <raal93@gmail.com>
2024-10-22 14:56:45 +0200

    added difficulties

--- script/script.js
+++ script/script.js
@@ -3,20 +3,31 @@ const gameContainer = document.getElementById("gameContainer");
 const topScoreContainer = document.getElementById("topScoreContainer");
 const historyUL = document.getElementById("historyUL");
 const endScore = document.getElementById("endScore");
+const easyBtn = document.getElementById("easy");
+const mediumBtn = document.getElementById("medium");
+const hardBtn = document.getElementById("hard");
+const randomBtn = document.getElementById("random");
 let score = 0;
 let timer = 10;
 let gameActive = false;
 let sqeareInterval;
 let topScore = localStorage.getItem("topScore");
-const history = [];
+let history = JSON.parse(localStorage.getItem("history"));
+let squareHeight = "100px";
+let squareWidth = "100px";
+let randomMode = false;
 
 setTopScore();
 setHistory();
 startBtn.addEventListener("click", startGame);
+easyBtn.addEventListener("click", setEasy);
+mediumBtn.addEventListener("click", setMedium);
+hardBtn.addEventListener("click", setHard);
+randomBtn.addEventListener("click", setRandom);
 
 function getRandomPositin() {
-  const maxX = gameContainer.clientWidth - 50;
-  const maxY = gameContainer.clientHeight - 50;
+  const maxX = gameContainer.clientWidth - parseInt(squareWidth);
+  const maxY = gameContainer.clientHeight - parseInt(squareHeight);
   const x = Math.floor(Math.random() * maxX);
   const y = Math.floor(Math.random() * maxY);
   return { x, y };
@@ -25,6 +36,16 @@ function getRandomPositin() {
 function createSquare() {
   const square = document.createElement("div");
   square.classList.add("square");
+  if (randomMode) {
+    squareHeight = getRandomSize();
+    squareWidth = getRandomSize();
+    square.style.height = squareHeight;
+    square.style.width = squareWidth;
+  } else {
+    square.style.height = squareHeight;
+    square.style.width = squareWidth;
+  }
+  square.style.background = getRandomGradient();
   const { x, y } = getRandomPositin();
   square.style.left = `${x}px`;
   square.style.top = `${y}px`;
@@ -93,12 +114,13 @@ function setTopScore() {
     topScore = score;
     topScoreContainer.textContent = `CORNGRATS! NEW TOP SCORE: ${topScore}`;
   } else {
-    topScoreContainer.textContent = `Top Score: ${topScore}`;
+    topScoreContainer.textContent = `Top Score: ${topScore || "0"}`;
   }
 }
 
 function updateHistory() {
   history.push(score);
+  if (history.length > 10) history.shift();
   setHistory();
 }
 
@@ -109,14 +131,79 @@ function showEndScore() {
 
 function updateLocalStorage() {
   localStorage.setItem("topScore", topScore);
-  localStorage.setItem("history", history);
+  localStorage.setItem("history", JSON.stringify(history));
 }
 
 function setHistory() {
   let ul = ``;
-  for (let i = history.length - 1; i >= 0; i--) {
-    ul += `<li>Score: ${history[i]}</li>`;
+  if (history) {
+    for (let i = history.length - 1; i >= 0; i--) {
+      ul += `<li>Score: ${history[i]}</li>`;
+    }
+  } else {
+    history = [];
   }
 
   historyUL.innerHTML = ul;
+}
+
+function setEasy() {
+  const EASY_SIZE = "200px";
+  squareWidth = EASY_SIZE;
+  squareHeight = EASY_SIZE;
+  randomMode = false;
+  mediumBtn.classList.remove("active");
+  hardBtn.classList.remove("active");
+  randomBtn.classList.remove("active");
+  easyBtn.classList.add("active");
+}
+
+function setMedium() {
+  const MEDIUM_SIZE = "100px";
+  squareWidth = MEDIUM_SIZE;
+  squareHeight = MEDIUM_SIZE;
+  randomMode = false;
+  easyBtn.classList.remove("active");
+  hardBtn.classList.remove("active");
+  randomBtn.classList.remove("active");
+  mediumBtn.classList.add("active");
+}
+
+function setHard() {
+  const HARD_SIZE = "50px";
+  squareWidth = HARD_SIZE;
+  squareHeight = HARD_SIZE;
+  randomMode = false;
+  easyBtn.classList.remove("active");
+  mediumBtn.classList.remove("active");
+  randomBtn.classList.remove("active");
+  hardBtn.classList.add("active");
+}
+
+function setRandom() {
+  randomMode = true;
+  easyBtn.classList.remove("active");
+  mediumBtn.classList.remove("active");
+  hardBtn.classList.remove("active");
+  randomBtn.classList.add("active");
+}
+
+function getRandomSize() {
+  return Math.floor(Math.random() * (500 - 5 + 1)) + 5 + "px";
+}
+
+function getRandomGradient() {
+  const red1 = Math.floor(Math.random() * 256);
+  const green1 = Math.floor(Math.random() * 256);
+  const blue1 = Math.floor(Math.random() * 256);
+  const alpha1 = (Math.random() * 0.7 + 0.3).toFixed(2);
+
+  const red2 = Math.floor(Math.random() * 256);
+  const green2 = Math.floor(Math.random() * 256);
+  const blue2 = Math.floor(Math.random() * 256);
+  const alpha2 = (Math.random() * 0.7 + 0.3).toFixed(2);
+
+  const angle = Math.floor(Math.random() * 360);
+
+  return `linear-gradient(${angle}deg, rgba(${red1}, ${green1}, ${blue1}, ${alpha1}), rgba(${red2}, ${green2}, ${blue2}, ${alpha2}))`;
 }
